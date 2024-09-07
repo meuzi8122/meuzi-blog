837ec56d3ad314753d47b2059c22bf1e01c6db51
meuzi8122 <kagiakete1234@gmail.com>
2024-09-07 20:08:13 +0900

    add daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require("daisyui"),
+		/* 静的コンポーネントスタイルを適用（TailwindCss標準） */
+		function ({ addComponents }) {
+			addComponents({
+			  ".container": {
+				maxWidth: "90%",
+				"@screen sm": {
+				  maxWidth: "600px",
+				},
+				"@screen md": {
+				  maxWidth: "700px",
+				},
+				"@screen lg": {
+				  maxWidth: "900px",
+				},
+				"@screen xl": {
+				  maxWidth: "1200px",
+				},
+			  },
+			});
+		  },
+	],
+	daisyui: {
+	  themes: ["sunset"],
+	},
 }
